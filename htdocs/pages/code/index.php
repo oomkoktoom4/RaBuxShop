bd6d07dbb93e465047502598dd0881746175b947
oomkoktoom4 <88439661+oomkoktoom4@users.noreply.github.com>
2022-09-22 23:47:48 +0700

    Add files via upload

---
+++ htdocs/pages/code/index.php
@@ -0,0 +1,21 @@
+<div class="container mt-3 text-center">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <img src="code.jpg" height="400" width="500">
+                    <div class="mt-2">
+                        <input class="form-control" id="pass_code" placeholder="กรุณากรอกรหัสโค๊ด">
+                    </div>
+                    <div class="mt-2">
+                        <p class="text-danger">Code คูปองแจกไว้หน้าเพจละค้าบ</p>
+                    </div>
+                    <div class="mt-2">
+                        <button type="button" class="btn btn-success w-100" id="submit_code">ตรวจสอบการทำรายการ</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="../../assets/js/code.js"></script>
---
+++ htdocs/pages/topup/index.php
@@ -0,0 +1,24 @@
+<div class="container mt-3 text-center">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <img src="https://cdn.discordapp.com/attachments/900746415238697001/973131814833127444/tw.png">
+                    <div class="mt-2">
+                        <input class="form-control" id="link_topup" placeholder="กรอกลิ้งซองอั่งเปา">
+                    </div>
+                    <div class="mt-2">
+                        <p class="text-danger">ตัวอย่างลิ้ง : https://gift.truemoney.com/campaign/?v=......</p>
+                        <p class="text-danger">เติมเงินขั้นตํ่า10บาทนะครับ</p>
+                        <div class="alert alert-danger text-sm text-center" role="alert">
+<i class="fas fa-exclamation-triangle fs-5"></i> แจ้งให้ทราบ ให้กรอกผู้รับเป็น1คนเท่านั้น เราไม่มีนโยบายการคืนเงินหรือถอนเงินออกจากเว็บกรุณาตรวจสอบให้ดีก่อนการชำระเงิน</div>
+                    </div>
+                    <div class="mt-2">
+                        <button type="button" class="btn btn-success w-100" id="submit_topup">ตรวจสอบการทำรายการ</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="../../assets/js/topup.js"></script>
